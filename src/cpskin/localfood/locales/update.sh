60c36a28cc74050e8c4a4643d5f123442cc5af16
Nicolas Demonte <daggelpop@gmail.com>
2018-03-27 14:46:17 +0200

    Fix unused project workflow translations

--- src/cpskin/localfood/locales/update.sh
+++ src/cpskin/localfood/locales/update.sh
@@ -10,3 +10,7 @@ domain=cpskin.localfood
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+
+# necessary for workflow translations
+i18ndude rebuild-pot --pot plone.pot --create plone ../profiles
+i18ndude sync --pot plone.pot */LC_MESSAGES/plone.po
